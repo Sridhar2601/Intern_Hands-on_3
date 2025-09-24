19a7414a0822218103ed237340f979dd5bf67784
Sridhar2601 <sridharchml@gmail.com>
2025-09-24 13:56:29 +0530

    mbl

--- src/web-router/Home.js
+++ src/web-router/Home.js
@@ -1,20 +1,54 @@
+// export default function Home() {
+//     return (
+//         <div>
+//             <div class="bg-primary text-white py-5">
+//                 <div class="container d-flex flex-column flex-md-row align-items-center justify-content-between">
+//                     <div>
+//                         <h1 class="ms-5 ps-5" style={{fontSize:'45px'}}>We provide <br /><u class="text-info">solutions</u><br />for your business
+//                             &<br />education!</h1>
+//                         <div class="d-flex gap-3 mt-3 ms-5 ps-5">
+//                             <button class="btn btn-info text-white">Get started</button>
+//                             <button class="btn btn-outline-light">Our services</button>
+//                         </div>
+//                     </div>
+//                     <img src="https://www.kreevtechsolutions.com/images/intro-img.svg" class="img-fluid mt-4 mt-md-0 me-2"
+//                         alt="Illustration" style={{maxWidth:"550px",height:'500px'}}></img>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
+
+
 export default function Home() {
     return (
         <div>
-            <div class="bg-primary text-white py-5">
-                <div class="container d-flex flex-column flex-md-row align-items-center justify-content-between">
-                    <div>
-                        <h1 class="ms-5 ps-5" style={{fontSize:'45px'}}>We provide <br /><u class="text-info">solutions</u><br />for your business
-                            &<br />education!</h1>
-                        <div class="d-flex gap-3 mt-3 ms-5 ps-5">
-                            <button class="btn btn-info text-white">Get started</button>
-                            <button class="btn btn-outline-light">Our services</button>
+            <div className="bg-primary text-white py-5">
+                <div className="container d-flex flex-column flex-md-row align-items-center justify-content-between">
+                    {/* Text Section */}
+                    <div className="text-center text-md-start px-3">
+                        <h1 style={{ fontSize: '2.5rem' }}>
+                            We provide <br />
+                            <u className="text-info">solutions</u><br />
+                            for your business &<br />education!
+                        </h1>
+                        <div className="d-flex flex-column flex-sm-row justify-content-center justify-content-md-start gap-3 mt-3">
+                            <button className="btn btn-info text-white">Get started</button>
+                            <button className="btn btn-outline-light">Our services</button>
                         </div>
                     </div>
-                    <img src="https://www.kreevtechsolutions.com/images/intro-img.svg" class="img-fluid mt-4 mt-md-0 me-2"
-                        alt="Illustration" style={{maxWidth:"550px",height:'500px'}}></img>
+
+                    {/* Image Section */}
+                    <div className="d-flex justify-content-center mt-4 mt-md-0">
+                        <img
+                            src="https://www.kreevtechsolutions.com/images/intro-img.svg"
+                            className="img-fluid"
+                            alt="Illustration"
+                            style={{ maxWidth: "100%", height: 'auto', maxHeight: '500px' }}
+                        />
+                    </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
--- src/web-router/Nav.js
+++ src/web-router/Nav.js
@@ -1,19 +1,66 @@
-import './CSS/Nav.css'
-export default function Nav()
-{
-    return(
-        <div className='main-one'>
-            <nav class="nb">
-                <div> <img src="https://www.kreevtechsolutions.com/images/image-removebg-preview.png" height="60px"
-                    width="300px" alt="company logo"></img></div>
-                <div class="n">
-                    <li> <a href="/">Home</a></li>
-                    <li> <a href="/About">About us</a></li>
-                    <li> <a href="/Service">Service</a></li>
-                    <li> <a href="/Product">Product</a></li>
-                    <li> <a href="/Contact">Contact us</a></li>
+// import './CSS/Nav.css'
+// export default function Nav()
+// {
+//     return(
+//         <div className='main-one'>
+//             <nav class="nb">
+//                 <div> <img src="https://www.kreevtechsolutions.com/images/image-removebg-preview.png" height="60px"
+//                     width="300px" alt="company logo"></img></div>
+//                 <div class="n">
+//                     <li> <a href="/">Home</a></li>
+//                     <li> <a href="/About">About us</a></li>
+//                     <li> <a href="/Service">Service</a></li>
+//                     <li> <a href="/Product">Product</a></li>
+//                     <li> <a href="/Contact">Contact us</a></li>
+//                 </div>
+//             </nav>
+//         </div>
+//     )
+// }
+
+
+import './CSS/Nav.css';
+import { useState } from 'react';
+
+export default function Nav() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <div className="main-one">
+            <nav className="nb d-flex flex-column flex-md-row justify-content-between align-items-center p-3">
+                {/* Logo */}
+                <div className="mb-2 mb-md-0 text-center text-md-start">
+                    <img
+                        src="https://www.kreevtechsolutions.com/images/image-removebg-preview.png"
+                        height="60"
+                        width="200"
+                        alt="company logo"
+                    />
                 </div>
+
+                {/* Hamburger Menu for Mobile */}
+                <div className="d-md-none text-end">
+                    <button
+                        className="btn btn-outline-dark"
+                        onClick={() => setIsOpen(!isOpen)}
+                    >
+                        ☰
+                    </button>
+                </div>
+
+                {/* Navigation Links */}
+                <ul
+                    className={`n list-unstyled d-flex flex-column flex-md-row align-items-center gap-3 mb-0 ${
+                        isOpen ? 'd-flex' : 'd-none d-md-flex'
+                    }`}
+                >
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/About">About us</a></li>
+                    <li><a href="/Service">Service</a></li>
+                    <li><a href="/Product">Product</a></li>
+                    <li><a href="/Contact">Contact us</a></li>
+                </ul>
             </nav>
         </div>
-    )
+    );
 }
